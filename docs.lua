36aad87bd0fc8939602c7a81bb8ca944e7b5281a
fali <baszucki906@gmail.com>
2025-12-23 10:28:40 +0300

    Add files via upload

---
+++ docs.lua
@@ -0,0 +1,2 @@
+local dl = dofile("dragonlang.lua")
+dl.output("Hello world!")
---
+++ dragonlang.lua
@@ -0,0 +1,8 @@
+local dragonlang = {}
+function dragonlang.output(text)
+    print(text)
+end
+function dragonlang.jellybeans()
+    print("Four hundred twenty five thousand three hundred and sixty two jelly beans")
+end
+return dragonlang
